725da910291cec1970a4af46ea1da05680f2c480
zcong1993 <1005997919@qq.com>
2018-01-25 16:00:34 +0800

    add test

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,57 @@
-const hotReload = require('../')
+const path = require('path')
+const fs = require('fs')
+const axios = require('axios')
+const run = require('../')
 
-test('main', () => {
-  expect(typeof hotReload).toBe('function')
+const entryPath = path.join(__dirname, 'fixture/app.js')
+const port = 9988
+const url = `http://localhost:${port}`
+const appContent = `
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(ctx => {
+  ctx.body = 'hello world'
+})
+
+app.listen(${port})
+`
+
+const modifyContent = `
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(ctx => {
+  ctx.body = 'hello koa'
+})
+
+app.listen(${port})
+`
+const defaultOpts = {
+  exec: entryPath,
+  context: entryPath,
+  cluster: 1
+}
+
+const delay = num => new Promise(resolve => {
+  setTimeout(() => resolve(), num)
+})
+
+beforeEach(() => {
+  fs.writeFileSync(entryPath, appContent)
+  run(defaultOpts)
+})
+
+afterEach(() => {
+  fs.unlinkSync(entryPath)
+})
+
+test('should reload after modify file', async () => {
+  await delay(1000)
+  const res = await axios.get(url)
+  expect(res.data).toBe('hello world')
+  fs.writeFileSync(entryPath, modifyContent)
+  await delay(1000)
+  const res2 = await axios.get(url)
+  expect(res2.data).toBe('hello koa')
 })
